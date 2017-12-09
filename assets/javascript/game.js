4058d8267b6497ef2e8776125073f5e6527d4627
KDarch928 <Kayle.Drucker@gmail.com>
2017-12-08 22:04:00 -0600

    set layout for hmtl

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//create var needed for the game
+
+var win = 0;
+var lose = 0;
+var guessLeft = 9;
+var guessSoFar = [];
+
+//create a counter function for win
+
+
+//create a counter function for lose
+
+
+//create a function for decressing guesses
+
+
+//create key event for the game
